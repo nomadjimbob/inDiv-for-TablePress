571a291b629f8df3a7723c76cdfe5d05312b4001
James Collins <james.collins@outlook.com.au>
2020-01-10 21:10:39 +1000

    Added License in Header

--- tablepress-indiv.php
+++ tablepress-indiv.php
@@ -3,9 +3,11 @@
 Plugin Name: TablePress Extension: InDiv
 Plugin URI: https://jamescollins.com.au/resources/tablepress-indiv
 Description: Custom Extension for TablePress to automatically wrap the table in a DIV element. Add in_div=true to your tables to enclose your TablePress tables in a DIV with the class tablepress_in_div.
-Version: 1.0
+Version: 1.0.1
 Author: James Collins
 Author URI: https://jamescollins.com.au/
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 
 /*
