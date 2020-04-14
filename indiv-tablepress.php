99f8426ec20d2f0fd92ef3627f90faf12f2a3df1
James Collins <james.collins@outlook.com.au>
2020-04-14 18:44:27 +1000

    Namespace fix

--- indiv-tablepress.php
+++ indiv-tablepress.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: TablePress Extension: InDiv
-Plugin URI: https://jamescollins.com.au/resources/tablepress-indiv
+Plugin Name: inDiv for TablePress
+Plugin URI: https://jamescollins.com.au/resources/indiv-tablepress/
 Description: Custom Extension for TablePress to automatically wrap the table in a DIV element. Add in_div=true to your tables to enclose your TablePress tables in a DIV with the class tablepress_in_div.
 Version: 1.0.1
 Author: James Collins
